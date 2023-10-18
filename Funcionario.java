5deb54ea511e09e5e90cecb81c35b5480fb4aedf
Larissa Gabrielle <105463088+LarissaGabrielle@users.noreply.github.com>
2023-10-17 21:11:57 -0300

    Create Funcionario.java
    add Funcionario

---
+++ Funcionario.java
@@ -0,0 +1,46 @@
+public class Funcionario {
+    private  int IdFunc;
+    private  String nome;
+    private  String email;
+    private  String documento;
+
+
+    public Funcionario(int idFunc, String nome, String email, String documento) {
+        IdFunc = idFunc;
+        this.nome = nome;
+        this.email = email;
+        this.documento = documento;
+    }
+
+    public int getIdFunc() {
+        return IdFunc;
+    }
+
+    public void setIdFunc(int idFunc) {
+        IdFunc = idFunc;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+}
